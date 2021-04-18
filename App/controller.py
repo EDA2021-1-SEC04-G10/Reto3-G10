47b68ade7dfd3906d10e29595332b848b4b8f044
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-04-17 23:03:01 -0500

    cambios en Controller

--- App/controller.py
+++ App/controller.py
@@ -26,13 +26,61 @@ import csv
 
 
 """
-El controlador se encarga de mediar entre la vista y el modelo.
+El controlador se encarga de mediar entre la vista y el modelo
 """
 
-# Inicialización del Catálogo de libros
+# Inicialización del analizador de eventos
+
+def initAnalyzer():
+    """
+    Llama la función de inicialización del analizador al modelo
+    """
+    analyzer = model.newAnalyzer()
+    return analyzer
 
 # Funciones para la carga de datos
 
-# Funciones de ordenamiento
+def loadData(analyzer):
+    """
+    Carga los datos de los archivos en las estructuras
+    de datos
+    """
+    loadEvents(analyzer)
 
-# Funciones de consulta sobre el catálogo
+def loadEvents(analyzer):
+    """
+    Carga los eventos del archivo. Por cada evento se indica al
+    modelo que debe adicionarlo al analizador
+    """
+    eventsfile = cf.data_dir + 'subsamples-small/context_content_features-small.csv'
+    input_file = csv.DictReader(open(eventsfile, encoding='utf-8'))
+    for event in input_file:
+        model.addEvent(analyzer, event)
+
+def loadTags(analyzer):
+    """
+    Carga las etiquetas del archivo. Por cada etiqueta se indica al
+    modelo que debe adicionarla al analizador
+    """
+    tagsfile = cf.data_dir + 'subsamples-small/user_track_hashtag_timestamp-small.csv'
+    input_file = csv.DictReader(open(tagsfile, encoding='utf-8'))
+    for tag in input_file:
+        model.addTag(analyzer, tag)
+
+def loadSentimentValues(analyzer):
+    """
+    Carga los valores de sentimientos del archivo. Por cada valor se
+    indica al modelo que debe adicionarlo al analizador
+    """
+    sentimentsfile = cf.data_dir + 'subsamples-samll/sentiment_values.csv'
+    input_file = csv.DictReader(open(sentimentsfile, encoding='utf-8'))
+    for sentiment in input_file:
+        model.addSentimentValue(analyzer, sentiment)
+
+# Funciones de consulta sobre el analizador
+
+def eventsSize(analyzer):
+    """
+    Número de eventos cargados en el analizador
+    """
+    return model.eventsSize(analyzer)
