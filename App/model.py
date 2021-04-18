420b70c234e89da8ef4a50178586347a2e7e9727
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-04-17 23:02:52 -0500

    cambios en Model

--- App/model.py
+++ App/model.py
@@ -18,33 +18,68 @@
  *
  * You should have received a copy of the GNU General Public License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contribuciones:
- *
- * Dario Correal - Version inicial
  """
-
 
 import config as cf
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
+from DISClib.ADT import orderedmap as om
 from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Sorting import shellsort as sa
 assert cf
 
+
 """
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
+Se define la estructura de un analizador de eventos. El analizador tendrá una lista para los eventos
+de escucha
 """
 
 # Construccion de modelos
 
-# Funciones para agregar informacion al catalogo
+def newAnalyzer():
+    """
+    Inicializa el analizador de eventos. Crea una lista para guardar
+    los eventos. Se crean índices (maps) por los siguientes
+    criterios:
 
-# Funciones para creacion de datos
+    """
+    analyzer = {'events': None}
+
+    """
+    Esta lista contiene los eventos del archivo. Los eventos son
+    referenciados por los índices creados a continuación
+    """
+    analyzer['events'] = lt.newList('ARRAY_LIST')
+
+    """
+    Se crean índices (maps) por diferentes criterios para llegar
+    a la información consultada
+    """
+
+    """
+    Este índice crea un map cuya llave es el
+    """
+
+    return analyzer
+
+# Funciones para agregar informacion al analizador
+
+def addEvent(analyzer, event):
+    """
+    Adiciona un evento a la lista de eventos
+    """
+    lt.addLast(analyzer['events'], event)
+
+# Funciones para creación de datos
 
 # Funciones de consulta
 
-# Funciones utilizadas para comparar elementos dentro de una lista
+def eventsSize(analyzer):
+    """
+    Número de videos cargados en el analizador
+    """
+    return lt.size(analyzer['events'])
+
+# Funciones utilizadas para comparar elementos
 
 # Funciones de ordenamiento
